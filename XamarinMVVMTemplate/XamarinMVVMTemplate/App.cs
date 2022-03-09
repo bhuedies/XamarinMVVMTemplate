973467b5dcf4150254462d07f209a6f788b50b3f
bhuedies <bhuedies@gmail.com>
2022-03-09 19:54:10 +0700

    Add project files.

---
+++ XamarinMVVMTemplate/XamarinMVVMTemplate/App.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using Caliburn.Micro.Xamarin.Forms;
+using Xamarin.Forms;
+using XamarinMVVMTemplate.Views;
+using XamarinMVVMTemplate.ViewModels;
+
+namespace XamarinMVVMTemplate
+{
+    public class App : FormsApplication
+    {
+        private readonly SimpleContainer container;
+
+        public App(SimpleContainer container)
+        {
+            Initialize();
+
+            this.container = container;
+
+            container.PerRequest<HomeViewModel>();
+
+            DisplayRootView<HomeView>();
+        }
+
+        protected override void PrepareViewFirst(NavigationPage navigationPage)
+        {
+            container.Instance<INavigationService>(new NavigationPageAdapter(navigationPage));
+        }
+    }
+}
---
+++ XamarinMVVMTemplate/XamarinMVVMTemplate/ViewModels/HomeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+
+namespace XamarinMVVMTemplate.ViewModels
+{    
+    public class HomeViewModel : Conductor<object>
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private string _txtName;
+
+        public string txtName
+        {
+            get { return _txtName; }
+            set
+            {
+                _txtName = value;
+                NotifyOfPropertyChange(() => txtName);
+            }
+        }
+        public HomeViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            //_eventAggregator.SubscribeOnPublishedThread(this);
+            txtName = "Dicoba dulu yak!!";
+        }
+    }    
+}
